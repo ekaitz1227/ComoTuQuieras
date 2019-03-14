5009396add972c986dc6d19f0b3844c44c0d5184
IN1dm3B_15 <IN1dm3B_15@HZ317743-PC.H015112.NET>
2019-03-14 12:40:56 +0100

    Empezemos

--- calculadora/src/calculadora/main.java
+++ calculadora/src/calculadora/main.java
@@ -1,7 +1,17 @@
 package calculadora;
 
-public class main {
-	int num1=0;
-	int num2=0;
+import java.util.Scanner;
 
+public class main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=0;
+		int num2=0;
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Introduce un numero: ");
+		num1= sc.nextInt();
+		System.out.println("Introduce otro numero: ");
+		num2= sc.nextInt();
+	}
 }
