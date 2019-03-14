f4ba8cc50d445f81544db5d53947108feae353f8
adrian <adrian.quintanaro@elorrieta-errekamari.com>
2019-03-14 12:55:24 +0100

    metodo resta realizado, numeros cambiado de 0,0 a 10,4

--- calculadora/src/calculadora/main.java
+++ calculadora/src/calculadora/main.java
@@ -5,13 +5,17 @@ import java.util.Scanner;
 public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int num1=0;
-		int num2=0;
+		int num1=10;
+		int num2=4;
 		Scanner sc = new Scanner (System.in);
 		
 		System.out.println("Introduce un numero: ");
 		num1= sc.nextInt();
 		System.out.println("Introduce otro numero: ");
 		num2= sc.nextInt();
+		resta(num1,num2);
+	}
+	public static void resta(int num1, int num2) {
+		int resta = num1 - num2;
 	}
 }
